346130f01f6dec30d3701950680b1f1d3ac612f4
flxk202122 <flxk202122@naver.com>
2023-11-03 17:56:45 +0900

    슬라이드 상하로 움직임, d-flex삭제, marginTop,높이 변경

--- js/web.js
+++ js/web.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     //화면이 준비되면 실행해라.
 
     setInterval(function () {
-        $(".swiper-wrapper").animate({ "marginLeft": -1200 }, 400, function () {
+        $(".swiper-wrapper").animate({ "marginTop": -300 }, 400, function () {
             //appendTo -> 나를 이동하는것.
             $("div:first-child", this).appendTo($(this))
-            $(this).css("marginLeft", 0)
+            $(this).css("marginTop", 0)
         })
     }, 3000)
 
