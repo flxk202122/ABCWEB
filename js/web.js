1ab47eeffcd55aeb68b64f05012555bc3a31d306
flxk202122 <flxk202122@naver.com>
2023-11-03 17:45:42 +0900

    슬라이드 시험용 완성

--- js/web.js
+++ js/web.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     //화면이 준비되면 실행해라.
 
+    setInterval(function () {
+        $(".swiper-wrapper").animate({ "marginLeft": -1200 }, 400, function () {
+            //appendTo -> 나를 이동하는것.
+            $("div:first-child", this).appendTo($(this))
+            $(this).css("marginLeft", 0)
+        })
+    }, 3000)
+
 })
